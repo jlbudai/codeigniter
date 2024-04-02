875779f183c0e2f431eb17fa6363e370c9457435
jlbudai <jlbudai@gmail.com>
2024-04-02 18:15:04 -0300

    first commit

---
+++ application/views/editor.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation for Sites</title>
+    <link rel="stylesheet" type="text/css" href="js/assets/content-tools.min.css">
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+    <div data-editable data-name="main-content">
+    <blockquote>
+        Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.
+    </blockquote>
+    <p>John F. Woods</p>
+	</div>
+	<script src="js/assets/content-tools.min.js"></script>
+    <script src="js/assets/editor.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
